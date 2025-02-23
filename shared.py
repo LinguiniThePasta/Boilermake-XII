4349162f8702d87810e0d0c0bfaa8319a6641bec
AndrewZacharyLiu <87785681+AndrewZacharyLiu@users.noreply.github.com>
2025-02-23 05:30:22 -0500

    HUGE SHIT AND TEMPO?

--- shared.py
+++ shared.py
@@ -1,6 +1,20 @@
 ''' FILE FOR CROSS SCRIPT VARIABLES???'''
 
 detection_events = None
+huge_shit = {}
+
+def get_huge_shit(key):
+    return huge_shit[key]
+
+def add_huge_shit(key, value):
+    global huge_shit
+    if key in huge_shit:
+        huge_shit[key].append(value)
+    else:
+        huge_shit[key] = [value]
+
+    print(huge_shit[key])
+
 def get_detection_events():
     return detection_events
 
